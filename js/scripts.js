9df925721652009649901cca3e1273f0e78c4588
Lauren Ackerland Yeiser & Spyder Monkee McHenry <student@epicodus.com>
2014-02-24 11:13:55 -0800

    adds new contact to page, shows contact when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ var Contact = {
 
 $(function(){
   $('form#new-contact').submit(function(event){
+    event.preventDefault();
     var inputFirstName = $('#new-first-name').val();
     var inputLastName = $('#new-last-name').val();
     var inputAddress = $('#new-address').val();
@@ -16,11 +17,19 @@ $(function(){
     currentContact.lastName = inputLastName;
     currentContact.address = inputAddress;
     
-    $('ul#contacts').append('<li><span class="contacts">' + currentContact.fullName() + '</span></li>');
-
-    event.preventDefault();
+    $('ul#contacts').append('<li><span class="contact">' + currentContact.fullName() + '</span></li>');
+  
+    $('.contact').last().click(function() {
+      $('#show-contact').show();
+      $('#show-contact h2').text(currentContact.fullName());
+      $('.first-name').text(currentContact.firstName);
+      $('.last-name').text(currentContact.lastName);
+      $('.address').text(currentContact.address);
+      
+    });
     this.reset();
-  });
+  }); 
+
 });
 
 
