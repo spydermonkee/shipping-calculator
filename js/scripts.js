44a4abb235452d7d132d488805829f07238b09cc
Lauren Ackerland Yeiser & Spyder Monkee McHenry <student@epicodus.com>
2014-02-24 14:55:39 -0800

    working triangle sorter with page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,55 @@
-var Contact = {
-  fullName:function(){
-    return this.firstName + " " + this.lastName;
+var Triangle = {
+  valid: function() {
+
+    if(this.sideA >= (this.sideB + this.sideC) || this.sideB >= (this.sideA + this.sideC) || this.sideC >= (this.sideA + this.sideB)){
+      return false;
+    } else {
+      return true;
+    }
+  },
+  triType: function(){
+    if((this.sideA === this.sideB) && (this.sideB === this.sideC)) {
+      return "equilateral";
+    } else if ((this.sideA === this.sideB) || (this.sideA === this.sideC) || (this.sideB === this.sideC)) {
+      return "isosceles";
+    } else {
+      return "scalene";
+    }
   }
 };
 
 $(function(){
+  $('form').submit(function(event){
+    event.preventDefault();
+    var sideA = parseFloat($('input#new-sideA').val());
+    var sideB = parseFloat($('input#new-sideB').val());
+    var sideC = parseFloat($('input#new-sideC').val());
+
+    var newTriangle = Object.create(Triangle);
+    newTriangle.sideA = sideA;
+    newTriangle.sideB = sideB;
+    newTriangle.sideC = sideC;
+  
+    if((isNaN(sideA) || sideA === 0) || (isNaN(sideB) || sideB === 0) || (isNaN(sideC) || sideC === 0)){
+      alert("Please enter positive numbers only!");
+    } else {
+      if(!newTriangle.valid()){
+        alert('These side lengths do not create a valid triangle...  SORRY!');
+      } else if(newTriangle.triType() === 'equilateral') {
+        $('ul#equilateral').append('<li>' + sideA + "," + sideB + "," + sideC + '</li>');
+      } else if(newTriangle.triType() === 'isosceles') {
+        $('ul#isosceles').append('<li>' + sideA + "," + sideB + "," + sideC + '</li>');
+      } else {
+        $('ul#scalene').append('<li>' + sideA + "," + sideB + "," + sideC + '</li>');
+      }
+    }  
+  });
+});
+
+
+
+
+/*$(function(){
   $('form#new-contact').submit(function(event){
     event.preventDefault();
     var inputFirstName = $('#new-first-name').val();
@@ -31,37 +76,20 @@ $(function(){
   }); 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*$(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
-
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedAddress = $("input#new-address").val();
-    var newContact = Object.create(Contact);
-    newContact.firstName = inputtedFirstName;
-    newContact.lastName = inputtedLastName;
-    newContact.address = inputtedAddress;
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    this.reset();
-  });
-});
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,41 @@
-describe('Contact', function(){
-  describe('fullName', function(){
-    it("combines the first and last name, separated by a space", function() {
-      var testContact = Object.create(Contact);
-      testContact.firstName = "Wonder";
-      testContact.lastName = "Boy";
-      testContact.fullName().should.equal("Wonder Boy");
+describe('Triangle', function(){
+  describe('valid', function() {
+    it('returns true if three lengths can create a triangle', function(){
+      var testTriangle = Object.create(Triangle);
+      testTriangle.sideA = 2;
+      testTriangle.sideB = 3;
+      testTriangle.sideC = 4;
+      testTriangle.valid().should.equal(true);
+    });
+    it('returns false if three lengths cannot create a triangle', function(){
+      var testTriangle = Object.create(Triangle);
+      testTriangle.sideA = 3;
+      testTriangle.sideB = 8;
+      testTriangle.sideC = 4;
+      testTriangle.valid().should.equal(false);
     });
   });
+  describe('triType', function(){
+    it('will return eqilateral if triangle passed is equilateral', function(){
+      var testTriangle = Object.create(Triangle);
+      testTriangle.sideA = 8;
+      testTriangle.sideB = 8;
+      testTriangle.sideC = 8;
+      testTriangle.triType().should.equal('equilateral');
+    })
+    it('will return "isosceles" if a triangle passed is an isoceles', function (){ 
+      var testTriangle = Object.create(Triangle);
+      testTriangle.sideA = 7;
+      testTriangle.sideB = 7;
+      testTriangle.sideC = 13; 
+      testTriangle.triType().should.equal('isosceles');
+    })
+   it('will return "scalene" if a triangle passed is a scalene triangle', function() {
+    var testTriangle = Object.create(Triangle);
+    testTriangle.sideA = 7;
+    testTriangle.sideB = 8;
+    testTriangle.sideC = 9;
+    testTriangle.triType().should.equal('scalene');
+   })
+  })
 });
