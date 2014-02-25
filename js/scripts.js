df9212538bfbf546d1f80f8161a1489f9d05e000
Lauren Ackerland Yeiser & Spyder Monkee McHenry <student@epicodus.com>
2014-02-24 17:30:08 -0800

    basic functionaity only

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,45 @@
-var Triangle = {
-  valid: function() {
+var Package = {
+  packageCalc: function(){
+    var packageRate = this.weight * 2;
+    if(this.dimensions > 100){
+      packageRate += 10;
+    } 
+    return packageRate;
+  }
+}
+var Shipment = {
+  getRate: function(){
+   var rate = 0;
+   var packageTotal = 0;
 
-    if(this.sideA >= (this.sideB + this.sideC) || this.sideB >= (this.sideA + this.sideC) || this.sideC >= (this.sideA + this.sideB)){
-      return false;
-    } else {
-      return true;
-    }
-  },
-  triType: function(){
-    if((this.sideA === this.sideB) && (this.sideB === this.sideC)) {
-      return "equilateral";
-    } else if ((this.sideA === this.sideB) || (this.sideA === this.sideC) || (this.sideB === this.sideC)) {
-      return "isosceles";
-    } else {
-      return "scalene";
-    }
+  if(this.distance > 100) {
+    rate += 3;
+  } else if(this.distance > 500) {
+  rate += (this.distance * .2)/100;
+  }
+  if(this.pickUp){
+    rate += 100;
+  }
+  if(this.signature){
+    rate += 50;
+  }
+  this.packages.forEach(function(pack){
+    console.log(pack);
+    rate += pack.packageCalc();
+  })
+  return rate;
   }
 };
 
-$(function(){
-  $('form').submit(function(event){
-    event.preventDefault();
-    var sideA = parseFloat($('input#new-sideA').val());
-    var sideB = parseFloat($('input#new-sideB').val());
-    var sideC = parseFloat($('input#new-sideC').val());
 
-    var newTriangle = Object.create(Triangle);
-    newTriangle.sideA = sideA;
-    newTriangle.sideB = sideB;
-    newTriangle.sideC = sideC;
-  
-    if((isNaN(sideA) || sideA === 0) || (isNaN(sideB) || sideB === 0) || (isNaN(sideC) || sideC === 0)){
-      alert("Please enter positive numbers only!");
-    } else {
-      if(!newTriangle.valid()){
-        alert('These side lengths do not create a valid triangle...  SORRY!');
-      } else if(newTriangle.triType() === 'equilateral') {
-        $('ul#equilateral').append('<li>' + sideA + "," + sideB + "," + sideC + '</li>');
-      } else if(newTriangle.triType() === 'isosceles') {
-        $('ul#isosceles').append('<li>' + sideA + "," + sideB + "," + sideC + '</li>');
-      } else {
-        $('ul#scalene').append('<li>' + sideA + "," + sideB + "," + sideC + '</li>');
-      }
-    }  
-  });
-});
+/*var package1 =
 
-
-
-
-/*$(function(){
-  $('form#new-contact').submit(function(event){
-    event.preventDefault();
-    var inputFirstName = $('#new-first-name').val();
-    var inputLastName = $('#new-last-name').val();
-    var inputAddress = $('#new-address').val();
-    
-     
-    var currentContact = Object.create(Contact);
-    currentContact.firstName = inputFirstName;
-    currentContact.lastName = inputLastName;
-    currentContact.address = inputAddress;
-    
-    $('ul#contacts').append('<li><span class="contact">' + currentContact.fullName() + '</span></li>');
-  
-    $('.contact').last().click(function() {
-      $('#show-contact').show();
-      $('#show-contact h2').text(currentContact.fullName());
-      $('.first-name').text(currentContact.firstName);
-      $('.last-name').text(currentContact.lastName);
-      $('.address').text(currentContact.address);
-      
-    });
-    this.reset();
-  }); 
-
-});
+var newShipment = Object.create(Shipment);
+newShipment.distance = 5000;
+newShipment.pickUp = false;
+newShipment.signature = false;
+newShipment.packages = [package1,package2];
 */
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,34 @@
-describe('Triangle', function(){
+
+describe('Shipment', function() {
+  describe('getRate', function () {
+    it('returns a rate from the weight of the package', function(){
+    
+    var currentPackage = Object.create(Package);
+      currentPackage.weight = 5;
+      currentPackage.dimensions = 10;
+     var testShipment = Object.create(Shipment);
+      testShipment.distance = 2050;
+      testShipment.pickUp = true;
+      testShipment.signature = true;
+      testShipment.packages = [currentPackage];
+      testShipment.getRate().should.equal(163);
+    });
+  });
+});
+
+describe('Package', function(){
+  describe('packageCalc', function(){
+    it('returns a price for the package', function(){
+      var newPackage = Object.create(Package);
+      newPackage.weight = 5;
+      newPackage.dimensions = 10;
+      newPackage.packageCalc().should.equal(10)
+    })
+  })
+})
+
+
+/*describe('Triangle', function(){
   describe('valid', function() {
     it('returns true if three lengths can create a triangle', function(){
       var testTriangle = Object.create(Triangle);
@@ -14,28 +44,5 @@ describe('Triangle', function(){
       testTriangle.sideC = 4;
       testTriangle.valid().should.equal(false);
     });
-  });
-  describe('triType', function(){
-    it('will return eqilateral if triangle passed is equilateral', function(){
-      var testTriangle = Object.create(Triangle);
-      testTriangle.sideA = 8;
-      testTriangle.sideB = 8;
-      testTriangle.sideC = 8;
-      testTriangle.triType().should.equal('equilateral');
-    })
-    it('will return "isosceles" if a triangle passed is an isoceles', function (){ 
-      var testTriangle = Object.create(Triangle);
-      testTriangle.sideA = 7;
-      testTriangle.sideB = 7;
-      testTriangle.sideC = 13; 
-      testTriangle.triType().should.equal('isosceles');
-    })
-   it('will return "scalene" if a triangle passed is a scalene triangle', function() {
-    var testTriangle = Object.create(Triangle);
-    testTriangle.sideA = 7;
-    testTriangle.sideB = 8;
-    testTriangle.sideC = 9;
-    testTriangle.triType().should.equal('scalene');
-   })
-  })
-});
+  });*/
+ 
